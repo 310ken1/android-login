c38a7dd7c94b58534159037f9a8b5595f058b130
Kenichi Sato <310dev@gmail.com>
2021-12-18 16:47:16 +0900

    バイブレーション、音再生サンプル

--- app/src/main/java/com/github/android_login/ui/main/MainFragment.java
+++ app/src/main/java/com/github/android_login/ui/main/MainFragment.java
@@ -1,6 +1,11 @@
 package com.github.android_login.ui.main;
 
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +15,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.github.android_login.R;
+import com.github.android_login.databinding.MainFragmentBinding;
 import com.github.android_login.ui.login.LoginFragment;
 import com.github.android_login.ui.login.LoginViewModel;
 
@@ -18,6 +23,7 @@ public class MainFragment extends Fragment {
     public static final String TAG = MainFragment.class.getSimpleName();
 
     private MainViewModel mViewModel;
+    private MainFragmentBinding binding;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -27,13 +33,26 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        binding = MainFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding.vibrator.setOnClickListener(v -> {
+            Vibrator vibrator = (Vibrator) requireActivity().getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(new long[]{0, 1000, 400, 200, 400, 200}, -1);
+        });
+
+        binding.alerm.setOnClickListener(v -> {
+            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone ringtone = RingtoneManager.getRingtone(requireContext(), uri);
+            ringtone.play();
+        });
+
+
     }
 
     @Override
